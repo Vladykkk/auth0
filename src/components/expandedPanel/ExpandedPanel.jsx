40df80a6aee641b7a18946ce55494aea99abe53b
Vladyslav <41453669+Vladykkk@users.noreply.github.com>
2024-04-28 19:11:40 +0000

    Features/task create help requests menu (#5)
    * feat: reduce bad code
    
    * feat: add expanded panel
    
    * fix naming

--- src/components/expandedPanel/ExpandedPanel.jsx
+++ src/components/expandedPanel/ExpandedPanel.jsx
@@ -1,35 +1,53 @@
-import React from 'react';
+import { useState } from "react";
+
 import "./expandedPanel.css";
 
-function ExpandedPanel({ expanded }) {
-    return (
-        <div className={expanded ? "expanded-panel show" : "expanded-panel"}>
-            <ul>
-                <li className="announcement-item">1</li>
-                <li className="announcement-item">2</li>
-                <li className="announcement-item">3</li>
-                <li className="announcement-item">1</li>
-                <li className="announcement-item">2</li>
-                <li className="announcement-item">3</li>
-                <li className="announcement-item">1</li>
-                <li className="announcement-item">2</li>
-                <li className="announcement-item">3</li>
-                <li className="announcement-item">1</li>   
-                <li className="announcement-item">2</li>
-                <li className="announcement-item">1</li>
-                <li className="announcement-item">2</li>
-                <li className="announcement-item">3</li>
-                <li className="announcement-item">1</li>
-                <li className="announcement-item">2</li>
-                <li className="announcement-item">3</li>
-                <li className="announcement-item">1</li>
-                <li className="announcement-item">2</li>
-                <li className="announcement-item">3</li>
-                <li className="announcement-item">1</li>   
-                <li className="announcement-item">2</li>
-            </ul>
+const ExpandedPanel = () => {
+  const [expanded, setExpanded] = useState(false);
+  const [items, setItems] = useState([]);
+  const [itemName, setItemName] = useState("");
+
+  const toggleExpanded = () => {
+    setExpanded(!expanded);
+  };
+
+  const addItem = () => {
+    if (itemName.trim() !== "") {
+      const newItem = itemName.trim();
+      setItems([...items, newItem]);
+      setItemName("");
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setItemName(e.target.value);
+  };
+
+  return (
+    <>
+      <button className="left-menu-button" onClick={toggleExpanded}></button>
+      <div className={expanded ? "expanded-panel show" : "expanded-panel"}>
+        <div className="form-container">
+          <input
+            type="text"
+            value={itemName}
+            onChange={handleInputChange}
+            placeholder="What is your problem?"
+          />
+          <button onClick={addItem}>+</button>
         </div>
-    );
-}
+        <ul>
+          <a href="#">
+            {items.map((item, index) => (
+              <li className="announcement-item" key={index}>
+                {item}
+              </li>
+            ))}
+          </a>
+        </ul>
+      </div>
+    </>
+  );
+};
 
 export default ExpandedPanel;
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,50 +1,34 @@
-import React, { useState } from 'react';
-import Auth from "../auth/Auth";
-import ExpandedPanel from "../expandedPanel/ExpandedPanel";
-import Logo from "/logo.jpg";
+import Header from "../header/Header";
+
 import HelpImage from "/help-image.svg";
-import HelpReqests from "../helpReqests/HelpReqests";
+
+import ExpandedPanel from "../expandedPanel/ExpandedPanel";
+import HelpRequests from "../helpRequests/HelpRequests";
 
 import "/src/reset.css";
 import "./home.css";
 
-
 function Home() {
-    const [expanded, setExpanded] = useState(false);
+  return (
+    <div className="App">
+      <Header />
+      <div className="main-content">
+        <h1 className="main-heading">
+          Need help? Find and offer services here!
+        </h1>
+        <p className="main-paragraph">
+          Welcome to our platform where people come together together to give or
+          receive help. Regardless of whether are you looking for professional
+          services or help with everyday needs, here you will find everything
+          you need!
+        </p>
 
-    const toggleExpanded = () => {
-        setExpanded(!expanded);
-    };
-
-    return (
-        <div className="App">
-            <header className="header">
-                <img src={Logo} alt="help-logo" className="help-logo" />
-                <div className="menu">
-                    <button className="add-button"> add an ad </button>
-                    <Auth />
-                </div>
-            </header>
-            <div className="main-content">
-                <h1 className="main-heading">
-                    Need help? Find and offer services here!
-                </h1>
-                <p className="main-paragraph">
-                    Welcome to our platform where people come together
-                    together to give or receive help. Regardless of whether
-                    are you looking for professional services or help with everyday needs,
-                    here you will find everything you need!
-                </p>
-                
-                <img src={HelpImage} alt="help-main-image" className="help-image" />
-                <button className="left-menu-button" onClick={toggleExpanded}></button>
-                <ExpandedPanel expanded={expanded} />
-                <HelpReqests />
-
-            
-            </div>
-        </div>
-    );
+        <img src={HelpImage} alt="help-main-image" className="help-image" />
+        <ExpandedPanel />
+        <HelpRequests />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
